9d5acc4a592926e38c514064d826ee6c3542040d
Jayden <sqwatato@gmail.com>
2023-08-15 21:41:07 -0700

    boilerplate

---
+++ opportunities/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Opportunity(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    gender = models.CharField(max_length=6, choices=[('Male', 'Male'), ('Female', 'Female')], blank=True)
+    size = models.CharField(max_length=6, choices=[('Small', 'Small'), ('Medium', 'Medium'), ('Large', 'Large')], blank=True)
+    description = models.TextField(blank=True)
+    shelter = models.ForeignKey('Shelter', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Shelter(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
