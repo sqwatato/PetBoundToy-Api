efd8b4fff4527aa199fbec51c66b8ad569ba9daa
Jayden <sqwatato@gmail.com>
2023-08-20 14:21:49 -0700

    add shelter api

--- opportunities/serializers.py
+++ opportunities/serializers.py
@@ -1,12 +1,13 @@
 from .models import Opportunity, Shelter
 from rest_framework.serializers import ModelSerializer
-
-class OpportunitySerializer(ModelSerializer):
-    class Meta:
-        model = Opportunity
-        fields = ['url', 'name', 'species', 'breed', 'age', 'gender', 'size', 'description', 'shelter', 'image', 'type']
         
 class ShelterSerializer(ModelSerializer):
     class Meta:
         model = Shelter
         fields = ['url', 'name', 'address', 'city', 'state', 'zip_code', 'phone_number']
+        
+class OpportunitySerializer(ModelSerializer):
+    shelter = ShelterSerializer(many=False, read_only=True)
+    class Meta:
+        model = Opportunity
+        fields = ['url', 'name', 'species', 'breed', 'age', 'gender', 'size', 'description', 'shelter', 'image', 'type']
